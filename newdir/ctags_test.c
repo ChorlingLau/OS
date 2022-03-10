5c7043d319885228c25e0dd3b593e1f5d78f7373
Chorling <20373453@buaa.edu.cn>
2022-03-10 16:30:55 +0800

    complete 0.1

---
+++ newdir/ctags_test.c
@@ -0,0 +1,6 @@
+# include "hello_world.c"
+
+int main() {
+	test_ctags();
+	return 0;
+}
---
+++ newdir/hello_world.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+int main() {
+	printf("Hello World!\n");
+	return 0;
+}
+
+void test_ctags() {
+	printf("testing ctags!\n");
+}
