08dc4898370ae1fae7514c0fb8e90aec31acabcf
Chorling <20373453@buaa.edu.cn>
2022-03-31 16:26:11 +0800

    2

--- my_cal/my_cal.c
+++ my_cal/my_cal.c
@@ -5,6 +5,20 @@ void _my_exit();
 
 void my_cal() {
 	char c;
+	unsigned int num=0;
 	c = _my_getchar();
-	_my_putchar(c);
+	while(c!='\n') {
+		num = num * 10 + c - '0';
+		c = _my_getchar();
+	}
+	int a[100]={0};
+	int len=0;
+	while(num){
+		a[len++]= num % 2;
+		num = num / 2;
+	}
+	while(len) {
+		c = a[len-1]+'0';
+		_my_putchar(c);
+	}
 }
