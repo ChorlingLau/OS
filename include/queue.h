93141b34654b31de5b9460dd933d5ca42f4d7a9c
Chorling <20373453@buaa.edu.cn>
2022-04-02 20:59:36 +0800

    complete 2.2

--- include/queue.h
+++ include/queue.h
@@ -109,7 +109,14 @@
  * already in the list.  The "field" name is the link element
  * as above.
  */
-#define LIST_INSERT_AFTER(listelm, elm, field)
+#define LIST_INSERT_AFTER(listelm, elm, field) do {												\
+				LIST_NEXT((elm),field) = LIST_NEXT((listelm), field);							\
+				if (LIST_NEXT((listelm), field) != NULL) {										\
+						LIST_NEXT((listelm), field)->field.le_prev = &LIST_NEXT((ELM), FIELD);	\
+				}																				\
+				LIST_NEXT((listelm), field) = (elm);											\
+				(elm)->field.le_prev = &LIST_NEXT((listelm), field);								\
+		} while (0)
         // Note: assign a to b <==> a = b
         //Step 1, assign elm.next to listelm.next.
         //Step 2: Judge whether listelm.next is NULL, if not, then assign listelm.next.pre to a proper value.
@@ -146,7 +153,18 @@
  * The "field" name is the link element as above. You can refer to LIST_INSERT_HEAD.
  * Note: this function has big differences with LIST_INSERT_HEAD !
  */
-#define LIST_INSERT_TAIL(head, elm, field)
+#define LIST_INSERT_TAIL(head, elm, field) do {														\
+				if (LIST_HEAD((head)) == NULL) {													\
+						LIST_INSERT_HEAD((head), (elm), field);										\
+				} else {																			\
+						LIST_NEXT((elm), field) = LIST_HEAD((head));								\
+						while (LIST_NEXT(LIST_NEXT((elm), field), field) != NULL) {					\
+								LIST_NEXT((elm), field) = LIST_NEXT(LIST_NEXT((elm), field), field);\
+						}																			\
+						LIST_NEXT(LIST_NEXT((elm), field), field) = (elm);							\
+						LIST_NEXT((elm), field) = NULL;												\
+				}																					\
+		} while (0)
 /* finish your code here. */
 
 
