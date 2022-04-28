ae508d19f0b33d171e33c09119bbe7dab0e0d22b
Chorling <20373453@buaa.edu.cn>
2022-04-28 16:26:51 +0800

    lab3-1-Extra

--- lib/env.c
+++ lib/env.c
@@ -45,9 +45,9 @@ int V(struct Env* e, int s) {
         return -1;
     }
 	signal[s-1]++;
-	e->env_res--;
-	if (e->env_res < 0) e->env_res = 0;
-	if (e->env_res) e->env_wait_status = 2;
+	if (e->env_res > 0) e->env_res--;
+
+	if (e->env_res > 0) e->env_wait_status = 2;
 	else e->env_wait_status = 3;
 	if (LIST_EMPTY(&env_wait_list)) {
 		return 0; 
