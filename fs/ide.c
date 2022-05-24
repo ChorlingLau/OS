52f17f78cdd7160dc8c68c59ee5217730cf89d15
Chorling <20373453@buaa.edu.cn>
2022-05-24 15:06:48 +0800

    debug of 5.1-5.3

--- fs/ide.c
+++ fs/ide.c
@@ -37,21 +37,22 @@ ide_read(u_int diskno, u_int secno, void *dst, u_int nsecs)
 		// Your code here
 		// error occurred, then panic.
 		int tmp_off = offset_begin + offset;
-        if (syscall_read_dev((u_int)&diskno, dev_addr + 0x10, 4) < 0)
+        if (syscall_write_dev((u_int)&diskno, dev_addr + 0x10, 4) < 0)
             user_panic("ide_read err 0x10\n");
-        if (syscall_read_dev((u_int)&tmp_off, dev_addr + 0x0, 4) < 0)
+        if (syscall_write_dev((u_int)&tmp_off, dev_addr + 0x0, 4) < 0)
             user_panic("ide_read err 0x0\n");
-        if (syscall_read_dev((u_int)(dst + offset), dev_addr + 0x4000, 0x200) < 0)
-            user_panic("ide_read err 0x4000\n");
-        if (syscall_read_dev((u_int)&read_value, dev_addr + 0x20, 1) < 0)
+        if (syscall_write_dev((u_int)&read_value, dev_addr + 0x20, 1) < 0)
             user_panic("ide_read err 0x20\n");
-        status = 0;
-        if (syscall_write_dev((u_int)&status, dev_addr + 0x30, 1) < 0)
-            user_panic("ide_read err 0x30\n");
 
+        status = 0;
+        if (syscall_read_dev((u_int)&status, dev_addr + 0x30, 1) < 0)
+            user_panic("ide_read err 0x30\n");
         if (!status)
             user_panic("ide_read failed!\n");
-        offset += 0x200;
+
+        if (syscall_read_dev((u_int)(dst + offset), dev_addr + 0x4000, 0x200) < 0)
+            user_panic("ide_read err 0x4000\n");
+		offset += 0x200;
 	}
 }
 
@@ -96,10 +97,10 @@ ide_write(u_int diskno, u_int secno, void *src, u_int nsecs)
 			user_panic("ide_write err 0x4000\n");
 		if (syscall_write_dev((u_int)&write_value, dev_addr + 0x20, 1) < 0)
 			user_panic("ide_write err 0x20\n");
-		status = 0;
-		if (syscall_write_dev((u_int)&status, dev_addr + 0x30, 1) < 0)
-			user_panic("ide_write err 0x30\n");
 		
+		status = 0;
+		if (syscall_read_dev((u_int)&status, dev_addr + 0x30, 1) < 0)
+			user_panic("ide_write err 0x30\n");
 		if (!status) 
 			user_panic("ide_write failed!\n");
 		offset += 0x200;
